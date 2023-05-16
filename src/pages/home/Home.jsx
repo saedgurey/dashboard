8b7529c792bb71ca4e11bf46ee4827030a61955c
saedgurey <saedgurey@gmail.com>
2023-05-16 19:08:37 +0300

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,36 @@
+import Chart from "../../components/chart/Chart";
+import Featured from "../../components/featured/Featured";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widget/Widget";
+import List from "../../components/table/List";
+import "./home.scss";
+import React from "react";
+
+
+const home = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured/>
+          <Chart/>
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transaction</div>
+     <List/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default home;
